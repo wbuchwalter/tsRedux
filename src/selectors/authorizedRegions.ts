55f6bb91995813feb6004b9bb8c2327b13fcb758
wbuchwalter <wbuchwalter@gmail.com>
2015-07-28 14:44:58 -0400

    Working selector

--- src/selectors/authorizedRegions.ts
+++ src/selectors/authorizedRegions.ts
@@ -1,12 +1,16 @@
 import reselect = require('reselect');
+import {Region} from '../reducers/region';
 
-const taxSelector = reselect.createSelector(
-  [state => state.regions, state => state.authorizedRegionUids],
+export const authorizeRegionsSelector = reselect.createSelector(
+  [state => state.regions.regionList, state => state.permissions.authorizedRegionUids],
   (regions, authorizedUids) => {
-    return _.map(regions, (r) => {
-      let region = angular.copy(r);
-      region.isAuthorized = _.any(authorizedUids, (uid) => uid === '*' || uid === region.uid);
-      return region; 
-    })
-  }
-);
+    return {
+      regionList: _.filter(regions, (r) => {
+        return _.any(authorizedUids, (uid) => uid === r.uid);       
+      })
+    };
+  });
+
+export interface AuthorizedRegionsSelectorData {
+  regionList: Region[];
+}
