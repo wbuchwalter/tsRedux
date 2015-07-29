8768380e4c2820abb94c325748d0d7f60b372c6d
William Buchwalter <wbuchwalter@gmail.com>
2015-07-28 20:16:04 -0400

    Dependencies

--- src/selectors/matchingRegions.ts
+++ src/selectors/matchingRegions.ts
@@ -5,14 +5,9 @@ import {authorizedRegionsSelector} from './authorizedRegions';
 export const matchingRegionsSelector = reselect.createSelector(
   [authorizedRegionsSelector, state => state.regions.filter],
   (regions: Region[], filter) => {
-    let regionList;
-
     if (!filter) {
-      console.log('no filter!');
       return regions;
     }
-    
-    console.log('filter!');
     return _.filter(regions, (r: Region) => { return r.name.toLowerCase().indexOf(filter.toLowerCase()) > -1; });
   });
 
